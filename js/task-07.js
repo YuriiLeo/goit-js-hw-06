ae915a666bac2e295c516a2fd6359c0de4cde675
YuriiLeo <yuriileontenko@gmail.com>
2022-07-07 17:29:14 +0300

    додав дефолтне зн

--- js/task-07.js
+++ js/task-07.js
@@ -8,6 +8,7 @@
 
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
+textEl.style.fontSize = "57px";
 inputEl.addEventListener("input", () => {
     textEl.style.fontSize = `${inputEl.value}px`;
 });
