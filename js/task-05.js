26b324738d968b1d93906797cc4a1b2a63ea4edf
YuriiLeo <yuriileontenko@gmail.com>
2022-07-07 17:29:28 +0300

    додав тернарник

--- js/task-05.js
+++ js/task-05.js
@@ -5,6 +5,17 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-});
+textInput.addEventListener("input", handleInputChange);
+
+// function handleInputChange(event) {
+//   if (event.currentTarget.value === "") {
+//     output.textContent = "Anonymous";
+//   } else {
+//     output.textContent = event.currentTarget.value;
+//   }
+// };
+ 
+function handleInputChange(event) {
+    output.textContent = event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
+  
+};
